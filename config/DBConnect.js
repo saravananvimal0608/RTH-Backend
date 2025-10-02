8c04e35c530b18585c1b886e587113c00b51ba64
saravananvimal0608 <saravananvimal0608@gmail.com>
2025-10-02 14:14:46 +0530

    Update DBConnect.js

--- config/DBConnect.js
+++ config/DBConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     try {
-        const MONGO_URI = process.env.MONGO_URI 
+        const MONGO_URI = "mongodb+srv://saravanan:saravanan8838144554@cluster0.chbhv4d.mongodb.net/RTH-EXERCISE?retryWrites=true&w=majority"
         const conn = await mongoose.connect(MONGO_URI)
         console.log("mongo conected");
     } catch (error) {
