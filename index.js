a6a06a3a49f6d81322f75969d981cb5270eb8e52
saravananvimal0608 <saravananvimal0608@gmail.com>
2025-10-02 14:05:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.get("/", (req, res) => {
     res.send("API Running")
 })
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
